85823dac4c4c62b6c616db63625885d5c6711995
EmmanuelJYokoyama <emmanuelnoronha2@gmail.com>
2023-03-22 18:49:41 -0300

    update 22/03 - mudanças com check box e filtro

--- app/src/main/java/com/example/projeto1obimestre/MainActivity.kt
+++ app/src/main/java/com/example/projeto1obimestre/MainActivity.kt
@@ -7,11 +7,7 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -21,30 +17,63 @@ import java.nio.charset.Charset
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+    lateinit var btn_finalizar: Button
+    lateinit var cb_P: CheckBox
+    lateinit var cb_R: CheckBox
+    lateinit var cb_S: CheckBox
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getProductWEB()
+        init()
+
+        getProductWEB(cb_P.isChecked, cb_R.isChecked, cb_S.isChecked)
+
+
+        cb_P.setOnCheckedChangeListener{buttonView, isChecked ->
+            getProductWEB(cb_P.isChecked, cb_R.isChecked, cb_S.isChecked)
+
+        }
+
+        cb_R.setOnCheckedChangeListener{buttonView, isChecked ->
+            getProductWEB(cb_P.isChecked, cb_R.isChecked, cb_S.isChecked)
+
+        }
+
+        cb_S.setOnCheckedChangeListener{buttonView, isChecked ->
+            getProductWEB(cb_P.isChecked, cb_R.isChecked, cb_S.isChecked)
+
+        }
+
     }
 
+    fun init(){
+        btn_finalizar = findViewById(R.id.btn)
+        cb_P = findViewById(R.id.check_Pizza)
+        cb_R = findViewById(R.id.check_Refri)
+        cb_S= findViewById(R.id.check_Sobre)
+    }
 
-    fun getProductWEB(){
+    fun getProductWEB(cbx_R: Boolean, cbx_P: Boolean, cbx_S: Boolean){
 
+        val line: LinearLayout = findViewById(R.id.linear)
         val queue = Volley.newRequestQueue(this)
         val url = "http://helioesperidiao.com/api.php"
         val requestBody = "id=1" + "&msg=test_msg"
-        val line: LinearLayout = findViewById(R.id.linear)
+
         val stringReq : StringRequest =
             object : StringRequest(Method.POST, url,
                 Response.Listener { response ->
 
-                var resposta = response.toString()
+                    var resposta = response.toString()
                     val array = JSONArray(resposta)
                     val tamanho =array.length()
                     for (i in 0 until tamanho ) {
                         val item: JSONObject = array.getJSONObject(i) // recupera o objeto na posição dentro do array
                         var idProduto = item.get("id").toString()
+                        var nome = item.get("nome").toString()
                         var type = item.get("type").toString();
                         var desc = item.get("desc").toString();
                         var preco = item.get("preco").toString();
@@ -53,18 +82,14 @@ class MainActivity : AppCompatActivity() {
                         var img = item.get("img").toString();
                         var novoTextView = TextView(this)
 
-                        novoTextView.text =  idProduto + " - " + type
-                        novoTextView.layoutParams = LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT)
+                        if(cbx_R || cbx_P || cbx_S){
+                            if((cbx_R && (type == " refrigerante")) || (cbx_P && (type == "pizza")) || (cbx_S && (type == "sobremesa")))
+                                views_dinamicas(idProduto, type, nome, desc, qtd, preco, img)
 
-                        line.addView(novoTextView)
+                        }else{
+                            views_dinamicas(idProduto, type, nome, desc, qtd, preco, img)
+                        }
 
-                        var novoimgview = ImageView(this)
-                        DownloadImageFromInternet(novoimgview).execute(img)
-                        novoimgview.layoutParams = LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT)
                     }
                 },
                 Response.ErrorListener { error ->
@@ -76,6 +101,73 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         queue.add(stringReq)
+
+    }
+
+    fun views_dinamicas(idProduto: String, type: String, nome:String, desc:String, qtd:String, preco:String, img:String){
+        val linearProdutos: LinearLayout = findViewById(R.id.linear)
+        linearProdutos.scrollY
+        var bloco = LinearLayout(this)
+
+        //bloco linear layout de produto
+        bloco.orientation = LinearLayout.VERTICAL
+
+
+        bloco.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        //criar imagem
+        var imagev = ImageView(this)
+        imagev.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        //carregar img
+        DownloadImageFromInternet(imagev).execute(img)
+
+        //texto do produto
+        var novoTextView = TextView(this)
+
+        novoTextView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        novoTextView.text = "${type} -> ${nome} R$ ${preco} \n "//${desc}
+
+        //botao para adicionar produto em pedido
+        val adicionar = Button(this)
+        adicionar.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        adicionar.text = "Adicionar ao pedido"
+        adicionar.setOnClickListener {
+            adicionarPedido()
+        }
+
+        bloco.addView(imagev)
+        bloco.addView(novoTextView)
+        bloco.addView(adicionar)
+
+        linearProdutos.addView(bloco)
+        var espaco = TextView(this)
+
+        espaco.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        espaco.text = "   "
+        espaco.height = 100
+        linearProdutos.addView(espaco)
+
+    }
+
+    fun adicionarPedido(){
+
+
+        var linearPedido: LinearLayout = findViewById(R.id.linear)
 
     }
 
